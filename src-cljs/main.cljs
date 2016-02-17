48269521b198ee072795b1958b853f4ab05bd0d2
romaintailhurat <romain.tailhurat@gmail.com>
2016-02-17 21:31:46 +0100

    Handling state with cljs hash-map and arrays

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -11,11 +11,18 @@
         :mars "mars"
       }}}})
 
+(def cards-1
+  {:player "1st"
+  :cards (array "blip" "blap" "bloup")})
+
 (defn slog [& m]
   "Simple logging tentative"
   (.log js/console (reduce str m)))
 
 (slog "S Y S T E M S")
+
+(slog (str cards-1))
+(slog (nth (get cards-1 :cards) 0))
 
 (defn title []
   [:div "S Y S T E M S"])
@@ -27,12 +34,17 @@
 (defn debug [input]
   [:div (str input)])
 
+(defn card [c]
+  [:div
+    (str c)])
+
 (defn app []
   [:div
     [title]
     [current-system (get state :current-system)]
-    [debug state]])
+    [debug cards-1]
+    [card (nth (get cards-1 :cards) 0)]])
 
 (defn ^:export run []
   (r/render [app]
-            (js/document.getElementById "app")))
+    (js/document.getElementById "app")))
